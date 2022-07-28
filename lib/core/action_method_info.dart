a0253256f1defd4998aa18c6eb9aec208f57ecb9
Nils ten Hoeve <nilstenhoeve@gmail.com>
2022-07-28 14:09:41 +0200

    Fixed #51

--- lib/core/action_method_info.dart
+++ lib/core/action_method_info.dart
@@ -8,15 +8,20 @@ import 'service_class_info.dart';
 
 /// ## [ActionMethod]
 ///
-/// [ActionMethod]s are methods in a [ServiceObject] or [DomainObject] that comply with a set of rules and are therefore recognized by the [ReflectFramework].
+/// [ActionMethod]s are methods in a [ServiceObject] or [DomainObject] that
+/// comply with a set of rules and are therefore recognized by the
+/// [ReflectFramework].
 ///
-/// [ActionMethod]s are displayed as menu items in a [ReflectGuiApplication] or as commands in other types of [ReflectApplication]s.
+/// [ActionMethod]s are displayed as menu items in a [ReflectGuiApplication]
+/// or as commands in other types of [ReflectApplication]s.
 ///
 /// A method needs to comply to the following rules to be considered a [ActionMethod] if:
 /// - the method is in a [ServiceObject] or [DomainObject]
 /// - and the method is public (method name does not start with an underscore)
-/// - and there is a [ActionMethodPreProcessor] that can process the method parameter signature.
-/// - and there is a [ActionMethodProcessor] that can process the method result.
+/// - and there is a [ActionMethodParameterProcessor]
+///   that can process the method parameter signature.
+/// - and there is a [ActionMethodResultProcessor]
+///   that can process the method result.
 ///
 
 abstract class ActionMethod extends ConceptDocumentation {}
